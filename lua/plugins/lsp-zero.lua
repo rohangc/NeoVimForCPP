7f6a76db9581fdcb480fe496ebc8504775cb6d69
rohangc <rohangc@gmail.com>
2024-10-13 16:14:55 +0530

    Some minor changes to: 'lua/plugins/lsp-zero.lua'

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -50,12 +50,12 @@ return{
             })
 
             require('mason').setup({})
-                require('mason-lspconfig').setup({
-                    handlers = {
-                        function(server_name)
-                            require('lspconfig')[server_name].setup({})
-                        end,
-                  },
+            require('mason-lspconfig').setup({
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                },
             })
 
             ----------------------------------------------------------------------------------
@@ -97,5 +97,5 @@ return{
                 },
             })
         end,
-    }
+    },
 }
