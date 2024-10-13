33e75ebc83da2ddeb7aa96618fc1912a4dbf8c60
rohangc <rohangc@gmail.com>
2024-10-13 14:09:11 +0530

    Two changes:
    1. Fixed a bug in lua/plugins/'nvim-tree.lua'.
    2. Brought back a couple of TagList settings from the old GVim configuration.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,17 +1,12 @@
 return {
     {'nvim-tree/nvim-tree.lua',
-         opt = {
+         opts = {
              sort = {sorter = 'case_sensitive'},
              view = {width = 60},
              renderer = {group_empty = true},
              filters = {dotfiles = true},
              git = {enable = false}
          },
-
-         -- Some sort of bug forces us to write this function (otherwise, 'opt' above would have sufficed) :
-         config = function()
-             require("nvim-tree").setup({})
-         end,
 
          keys = {
              {'<F10>', ':NvimTreeToggle<CR>', { noremap = true, silent = true, mode='n' }},
--- lua/plugins/taglist.lua
+++ lua/plugins/taglist.lua
@@ -1,7 +1,13 @@
 return {
     {'yegappan/taglist',
+
         keys = {
             {'<F9>', ':TlistToggle<CR>', { noremap = true, silent = true, mode = 'n' }},
         },
+
+        init = function()
+            vim.g.Tlist_Inc_Winwidth = 0
+            vim.g.Tlist_WinWidth = 60
+        end,
     },
 }
