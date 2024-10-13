1b8c4f13cee17158d7b496199ad38eff922531cd
rohangc <rohangc@gmail.com>
2024-10-13 12:51:50 +0530

    Changed the leader key from: '\' to: ' '.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,8 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
