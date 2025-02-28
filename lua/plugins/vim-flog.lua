2e4d77dbf164cac0f3a2363ee35b58c63616629e
rohangc <rohangc@gmail.com>
2025-02-28 23:33:31 +0530

    Replaced Fugitive with Neogit (although Flog keeps it around as a dependency).

--- lua/plugins/vim-flog.lua
+++ lua/plugins/vim-flog.lua
@@ -1,7 +1,8 @@
 return {
   "rbong/vim-flog",
   lazy = true,
-  cmd = {"Flog", "Flogsplit", "Floggit"},
+  --cmd = {"Flog", "Flogsplit", "Floggit"},
+  cmd = {"Flog"},
   dependencies = {
     "tpope/vim-fugitive",
   },
