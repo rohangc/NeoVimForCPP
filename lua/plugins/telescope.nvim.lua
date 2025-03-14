c17418e866a424d550a6ee8d14fb5b5cddbcbdae
Rohan Goud <rohang@altair.com>
2025-03-14 10:16:11 +0530

    Added two new Telescope shortcuts:
    1. \fd -> Diagnostics list.
    2. \ft  -> Tag stack.
    
    Also added help texts for each keybinding so that they show-up correctly in help texts populated by: 'whichkey'.

--- lua/plugins/telescope.nvim.lua
+++ lua/plugins/telescope.nvim.lua
@@ -2,10 +2,12 @@ return {
     {'nvim-telescope/telescope.nvim',
         init = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
+            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {desc = 'Telescope diagnostics'})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
+            vim.keymap.set('n', '<leader>ft', builtin.tagstack, {desc = 'Telescope tag stack'})
         end,
     },
 }
