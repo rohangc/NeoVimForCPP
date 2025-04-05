9e5625cf0bbca02671c43f1221af70a23f76d102
Rohan Goud <rohang@altair.com>
2025-04-05 19:41:41 +0530

    Made changes for Neovim 0.11.0

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- Emulate Windows behaviour also for cut/copy/paste
-vim.cmd("source $VIMRUNTIME/mswin.vim")
+local nvim_version = vim.version()
+if nvim_version.major == 0 and nvim_version.minor <= 10 then
+    vim.cmd("source $VIMRUNTIME/mswin.vim")
+else
+    vim.cmd("source $VIMRUNTIME/scripts/mswin.vim")
+end
 
 -- Load global options
 require('config.options')
