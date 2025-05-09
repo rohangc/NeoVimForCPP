c504385b32cbe711834939975e5b3583421402e3
Rohan Goud <rohang@altair.com>
2025-05-09 08:04:24 +0530

    Completely re-written LSP and CMP integration - now vastly improved.

--- lua/plugins/telescope.nvim.lua
+++ lua/plugins/telescope.nvim.lua
@@ -1,13 +1,37 @@
 return {
-    {'nvim-telescope/telescope.nvim',
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-ui-select.nvim',
+            {'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font}
+        },
+
+        opts = {
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown {
+                        -- even more opts
+                    }
+                }
+            }
+        },
+
         init = function()
+            local telescope = require('telescope')
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
-            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {desc = 'Telescope diagnostics'})
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
-            vim.keymap.set('n', '<leader>ft', builtin.tagstack, {desc = 'Telescope tag stack'})
+
+            -- Load the ui-select extension
+            telescope.load_extension('ui-select')
+
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope [B]uffers'})
+            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {desc = 'Telescope [D]iagnostics'})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope [F]iles'})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live [G]rep'})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope [H]elp tags'})
+            vim.keymap.set('n', '<leader>fk', builtin.keymaps, {desc = 'Telescope [K]eymaps'})
+            vim.keymap.set('n', '<leader>ft', builtin.tagstack, {desc = 'Telescope [T]ag stack'})
+            vim.keymap.set('n', '<leader>fW', builtin.grep_string, { desc = 'Telescope current [W]ord'})
         end,
     },
 }
