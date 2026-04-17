90b308804bad7a9a89d4c7d3ab60f930f66ef597
rohangc <rohangc@gmail.com>
2026-04-17 16:10:49 +0530

    We can now specify a list of excluded languages for treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -28,12 +28,16 @@ return {
       end
 
       local available_parsers = require('nvim-treesitter').get_available()
+      local excluded_languages = { 'fortran' } -- add languages to ignore here
+
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
           local buf, filetype = args.buf, args.match
 
           local language = vim.treesitter.language.get_lang(filetype)
           if not language then return end
+
+          if vim.tbl_contains(excluded_languages, language) then return end
 
           local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
 
