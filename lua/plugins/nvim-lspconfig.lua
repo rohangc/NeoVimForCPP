01524a85c7784c7d77da2260b5c07bd58849ef6d
Rohan Goud <rohang@altair.com>
2026-08-06 18:46:09 +0530

    Enabled support for Github Copilot.
    The following new plugins were added:
    
    1. 'zbirenbaum/copilot.lua'.
    2. 'CopilotC-Nvim/CopilotChat.nvim'.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -303,10 +303,10 @@ return {
       }
 
       -- Github Copilot support:
---    require('copilot').setup({
---      auto_attach = true,
---      suggestion = { enabled = false },
---      panel = { enabled = false },
---    })
+      require('copilot').setup({
+        auto_attach = true,
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
     end,
 }
